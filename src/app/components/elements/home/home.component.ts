21d20d7a416df9d512a18ddd839b9cb3d80c4ba1
wahajreh <67073708+wahajreh@users.noreply.github.com>
2024-02-14 23:51:13 +0500

    sdsds

--- src/app/components/elements/home/home.component.ts
+++ src/app/components/elements/home/home.component.ts
@@ -5,7 +5,22 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
- 
+  animations: [
+    trigger('slideInOut', [
+      state('open', style({
+        transform: 'translateX(0)'
+      })),
+      state('closed', style({
+        transform: 'translateX(100%)'
+      })),
+      transition('open => closed', [
+        animate('3.7s')
+      ]),
+      transition('closed => open', [
+        animate('3.7s')
+      ])
+    ])
+  ]
 })
 export class HomeComponent {
   isOpen = false;
